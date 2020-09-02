20d9e67ba255ac7849cf452da3e52ee06fe4151d
Agustín Quintanilla <aqrojo@oneclick.es>
2020-09-01 22:48:01 -0300

    ...

--- build/assets/home.js
+++ build/assets/home.js
@@ -1,5 +1,47 @@
-const contactTag = document.getElementById('contact')
+// --------------------------------
+// menu intersection
+const intersectionOptions = { rootMargin: '100px 0px 0px' };
+const intersectionTrigger = document.querySelector('#projects');
+const menu = document.querySelector('#nav');
 
-function goToContact () {
+function intersectionCallback(entries, observer) {
+  if (entries[0].isIntersecting) {
+    menu.classList.add('solid-header');
+  } else {
+    menu.classList.remove('solid-header');
+  }
+}
+
+const observer = new IntersectionObserver(intersectionCallback, intersectionOptions);
+observer.observe(intersectionTrigger);
+
+// --------------------------------
+// contact button
+const contactTag = document.getElementById('contact');
+
+function goToContact() {
   contactTag.scrollIntoView();
 }
+
+// --------------------------------
+// projects view
+const mySwiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
---
+++ build/assets/nav.js
@@ -0,0 +1,3 @@
+function nav_toggleMenu() {
+  document.querySelector('.nav-menu').classList.toggle('open');
+}
